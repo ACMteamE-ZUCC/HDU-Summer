5b2125b1a43a8e09781435dad814809c0e83a142
Andor233 <andor.liu@gmail.com>
2016-07-26 12:34:31 +0800

    c

---
+++ a.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+#include<cstring>
+using namespace std;
+typedef long long ll;
+char s[1000];
+ll n;
+int imain(void)
+{
+	if(strlen(s)>=18){
+		 cout<<"TAT"<<'\n';
+		 return 0;
+	}
+	else{
+		sscanf(s,"%lld",&n);
+	}
+	int cnt=0;
+	while(n!=1){
+		if(cnt>5){
+			cout<<"TAT"<<'\n';
+			return 0;
+		}
+		n=(ll)sqrt(n);
+		cnt++;
+	}
+	cout<<cnt<<'\n';
+	return 0;
+}
+int main(void)
+{
+	while(cin>>s){
+		imain();
+	}
+}
